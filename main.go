afc22903791b517860c3d2d69979e9ac886a28a0
rahul personal <rahulkum94l94@gmail.com>
2026-05-25 15:51:30 +0530

    feat:Ping Pong added

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"bufio"
+	"strings"
 )
 
 func main(){
@@ -20,5 +22,30 @@ func main(){
 			continue
 		}
 		fmt.Println("Client Connected",conn.RemoteAddr())
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn){
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	
+	for {
+		message,err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading message",conn.RemoteAddr(),err)
+			return
+		}
+
+		message = strings.TrimSpace(message)
+
+		fmt.Println("Message received",message)
+
+		if message == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}else {
+			conn.Write([]byte("-ERR unknown command\r\n"))
+		}
 	}
 }
