3c160f1421007a886c1eb05340ea1b8e9725ac4d
rahul personal <rahulkum94l94@gmail.com>
2026-05-25 16:18:52 +0530

    feat:added Basic Resp Parser

--- server/handler.go
+++ server/handler.go
@@ -1,11 +1,38 @@
 package server
 
-func HandleCommand(command string) string {
+import (
+	"strconv"
+	"strings"
+)
+
+func HandleCommand(message string) string {
+
+	parts := strings.Split(message, " ")
+
+	if len(parts) == 0 {
+		return "-ERR empty command\r\n"
+	}
+
+	command := strings.ToUpper(parts[0])
 
 	switch command {
 
 	case "PING":
 		return "+PONG\r\n"
+
+	case "ECHO":
+
+		if len(parts) < 2 {
+			return "-ERR wrong number of arguments for 'ECHO'\r\n"
+		}
+
+		msg := strings.Join(parts[1:], " ")
+
+		return "$" +
+			strconv.Itoa(len(msg)) +
+			"\r\n" +
+			msg +
+			"\r\n"
 
 	default:
 		return "-ERR unknown command\r\n"
