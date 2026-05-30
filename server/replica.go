e2b61575ef91f8e5b48fe8ab7622e72a852079ff
rahul personal <rahulkum94l94@gmail.com>
2026-05-30 19:37:47 +0530

    feat:added handling full sync

--- server/replica.go
+++ server/replica.go
@@ -4,26 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
+
 	"github.com/rahulCoder9417/Redis-in-go/server/config"
 )
 
-func ConnectToMaster(){
-	address:=
+func ConnectToMaster() {
+
+	address :=
 		config.ServerConfig.MasterHost +
-			":" + 
+			":" +
 			config.ServerConfig.MasterPort
-	
+
 	conn, err := net.Dial(
 		"tcp",
 		address,
 	)
 
-	if err != nil{
+	if err != nil {
+
 		fmt.Println(
-			"failed Connecting master:", err,
+			"failed connecting master:",
+			err,
 		)
+
 		return
 	}
+
 	fmt.Println(
 		"connected to master:",
 		address,
@@ -32,11 +40,17 @@ func ConnectToMaster(){
 	reader :=
 		bufio.NewReader(conn)
 
+	// 1 PING
+
 	SendCommand(
 		conn,
 		reader,
-		[]string{"PING"},
+		[]string{
+			"PING",
+		},
 	)
+
+	// 2 REPLCONF listening-port
 
 	SendCommand(
 		conn,
@@ -48,6 +62,8 @@ func ConnectToMaster(){
 		},
 	)
 
+	// 3 REPLCONF capa psync2
+
 	SendCommand(
 		conn,
 		reader,
@@ -58,14 +74,42 @@ func ConnectToMaster(){
 		},
 	)
 
-	SendCommand(
-		conn,
-		reader,
-		[]string{
-			"PSYNC",
-			"?",
-			"-1",
-		},
+	// 4 PSYNC
+
+	conn.Write(
+		[]byte(
+			EncodeRESP(
+				[]string{
+					"PSYNC",
+					"?",
+					"-1",
+				},
+			),
+		),
+	)
+
+	err =HandleFullResync(
+			reader,
+		)
+
+	if err != nil {
+
+		fmt.Println(
+			"fullresync failed:",
+			err,
+		)
+
+		return
+	}
+
+	fmt.Println(
+		"stored replication id:",
+		config.ServerConfig.ReplicationID,
+	)
+
+	fmt.Println(
+		"stored offset:",
+		config.ServerConfig.ReplicationOffset,
 	)
 
 	fmt.Println(
@@ -103,4 +147,67 @@ func SendCommand(
 		"master:",
 		response,
 	)
+}
+
+func HandleFullResync(
+	reader *bufio.Reader,
+) error {
+
+	line, err :=
+		reader.ReadString('\n')
+
+	if err != nil {
+
+		return err
+	}
+
+	line =
+		strings.TrimSpace(
+			line,
+		)
+
+	fmt.Println(
+		"master:",
+		line,
+	)
+
+	parts :=
+		strings.Split(
+			line,
+			" ",
+		)
+
+	if len(parts) != 3 {
+
+		return fmt.Errorf(
+			"invalid FULLRESYNC response",
+		)
+	}
+
+	if parts[0] != "+FULLRESYNC" {
+
+		return fmt.Errorf(
+			"expected FULLRESYNC",
+		)
+	}
+
+	config.ServerConfig.ReplicationID =
+		parts[1]
+
+	offset, err :=
+		strconv.ParseInt(
+			parts[2],
+			10,
+			64,
+		)
+
+	if err != nil {
+
+		return err
+	}
+
+	config.ServerConfig.ReplicationOffset =
+		offset
+
+	return nil
 }
