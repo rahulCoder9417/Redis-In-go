8c12a8afb6484673c26562062be4a99de8e9025e
rahul personal <rahulkum94l94@gmail.com>
2026-05-30 17:13:34 +0530

    faet:Added Basic Config

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/rahulCoder9417/Redis-in-go/server"
+import (
+	"github.com/rahulCoder9417/Redis-in-go/server"
+	"os"
+)
 
 func main() {
+
+	server.InitConfig()
+
+	args := os.Args
+
+	if len(args) >= 4 && args[1] == "--replicaof" {
+		server.ServerConfig.IsReplica = true
+		server.ServerConfig.MasterHost = args[2]
+		server.ServerConfig.MasterPort = args[3]
+	}
+
 	server.Start()
 }
---
+++ server/config.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+type Config struct {
+	Port string
+
+	IsReplica bool
+
+	MasterHost string
+	MasterPort string
+
+	ReplicationID string
+	ReplicationOffset int64
+}
+
+var ServerConfig Config
+
+func InitConfig() {
+
+	ServerConfig = Config{
+		Port: "6380",
+
+		IsReplica: false,
+
+		ReplicationID: RandomID(),
+
+		ReplicationOffset: 0,
+	}
+}
+
+func RandomID() string {
+
+	bytes := make([]byte, 20)
+
+	rand.Read(bytes)
+
+	return hex.EncodeToString(bytes)
+}
