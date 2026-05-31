a5bc9cb8649db2bc43dd9cd0d17028b68fea3274
rahul personal <rahulkum94l94@gmail.com>
2026-05-31 11:38:10 +0530

    feat:functions created to send db snapshots

--- server/commands/replications.go
+++ server/commands/replications.go
@@ -3,6 +3,10 @@ package commands
 import (
 	"strconv"
 	"github.com/rahulCoder9417/Redis-in-go/server/config"
+	"bufio"
+	"io"
+	"strings"
+	"net"
 )
 
 func ReplConf(parts []string)string{
@@ -26,4 +30,83 @@ func PSync(parts []string)string{
 			"\r\n"
 
 	return response
+}
+
+
+
+func SendEmptyRDB(conn net.Conn)error {
+	emptyRDB := []byte{
+		0x52,
+		0x45,
+		0x44,
+		0x49,
+		0x53,
+		0x30,
+		0x30,
+		0x31,
+		0x31,
+	}
+
+	header :=
+		"$" +
+		strconv.Itoa(
+			len(emptyRDB),
+		) +
+		"\r\n"
+
+	_, err :=
+		conn.Write(
+			[]byte(header),
+		)
+
+	if err != nil {
+		return err
+	}
+
+	_, err =
+		conn.Write(
+			emptyRDB,
+		)
+
+	return err
+}
+
+func DiscardRDB(
+	reader *bufio.Reader,
+) error {
+
+	line, err :=
+		reader.ReadString('\n')
+
+	if err != nil {
+		return err
+	}
+
+	line =
+		strings.TrimSpace(line)
+
+	size, err :=
+		strconv.Atoi(
+			line[1:],
+		)
+
+	if err != nil {
+		return err
+	}
+
+	buf :=
+		make(
+			[]byte,
+			size,
+		)
+
+	_, err = io.ReadFull(reader, buf)
+
+	if err != nil {
+		return err
+	}
+
+	reader.ReadString('\n')
+
+	return nil
 }
